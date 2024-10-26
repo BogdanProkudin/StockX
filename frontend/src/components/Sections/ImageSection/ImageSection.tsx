1209836aa8bc2cff5916b30e4d8667715f6535f5
HrafovMaksym <mgrafov06@gmail.com>
2024-10-26 22:44:13 +0200

    add ignore env and add section logic

--- frontend/src/components/Sections/ImageSection/ImageSection.tsx
+++ frontend/src/components/Sections/ImageSection/ImageSection.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ImageCard from "../../Cards/ImageCard/ImageCard";
 import { cardAssets } from "../../../assets/ImgSection/ImgSection";
+
 const ImageSection: React.FC = () => {
   return (
-    <div className="mb-20">
+    <div className="mb-10">
       <h1 className="my-5 font-bold text-xl ">Holiday Gift Guides</h1>
 
       <div className="flex justify-between">
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,22 +6,39 @@ import {
   userSectionFetch,
 } from "../redux/slices/homeItemsSlice";
 import { useAppDispatch, useAppSelector } from "../redux/hook";
+import { useInView } from "react-intersection-observer";
 
 import UserSection from "../components/Sections/UserSection/UserSection";
 import MainSection from "../components/Sections/MainSection/MainSection";
 import ImageSection from "../components/Sections/ImageSection/ImageSection";
+
 const Home: React.FC = () => {
   const dispatch = useAppDispatch();
-  const { recentlyViewedItems, recomendedItems, mainItems } = useAppSelector(
+  const { recentlyViewedItems, recomendedItems, mainSection } = useAppSelector(
     (state) => state.homeItems
   );
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+  const { ref: ref2, inView: inView2 } = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
   React.useEffect(() => {
     dispatch(userSectionFetch());
   }, []);
   React.useEffect(() => {
-    dispatch(mainSectionFetch());
-  }, []);
-  console.log(mainItems);
+    if (inView) {
+      dispatch(mainSectionFetch(1));
+    }
+  }, [inView]);
+  React.useEffect(() => {
+    if (inView2) {
+      dispatch(mainSectionFetch(2));
+    }
+  }, [inView2]);
 
   return (
     <div className="mt-6">
@@ -36,12 +53,23 @@ const Home: React.FC = () => {
         items={recomendedItems.data}
         description={recomendedItems.description}
       />
-      <MainSection
-        mainTitle={mainItems.title}
-        items={mainItems.data}
-        description={mainItems.description}
-      />
       <ImageSection />
+      <div ref={ref}>
+        <MainSection
+          status={mainSection[1].status}
+          mainTitle={mainSection[1].data.title}
+          items={mainSection[1].data.data}
+          description={mainSection[1].data.description}
+        />
+      </div>
+      <div ref={ref2}>
+        <MainSection
+          status={mainSection[2].status}
+          mainTitle={mainSection[2].data.title}
+          items={mainSection[2].data.data}
+          description={mainSection[2].data.description}
+        />
+      </div>
     </div>
   );
 };
