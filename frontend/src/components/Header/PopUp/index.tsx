df6818899b159811dfa4acfa57416c1dc3faa59a
Богдан Прокудин <prokudinbogdan50@gmail.com>
2024-10-29 17:23:51 +0200

    icons added

--- frontend/src/components/Header/PopUp/index.tsx
+++ frontend/src/components/Header/PopUp/index.tsx
@@ -16,6 +16,8 @@ const index: React.FC<PopupProps> = ({ objectArr }) => {
     <ul className={styles.header_popUp_link_container}>
       {objectArr?.map((obj) => (
         <div key={obj.name} className={styles.header_popUP_link_wrapper}>
+          <span>{obj.img}</span>
+
           <li
             onClick={obj.name === "Log Out" ? onClickLogout : () => {}}
             className={styles.header_popUP_link}
