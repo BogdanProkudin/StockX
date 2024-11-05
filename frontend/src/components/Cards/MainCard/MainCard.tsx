7393a7bfbf644ee6dce7f0b106a54d8f6bff01c9
HrafovMaksym <mgrafov06@gmail.com>
2024-11-05 23:03:25 +0100

    new

--- frontend/src/components/Cards/MainCard/MainCard.tsx
+++ frontend/src/components/Cards/MainCard/MainCard.tsx
@@ -15,22 +15,22 @@ const UserCard: React.FC<userCardProps> = ({
   };
   return (
     <Link
-      to=""
-      className=" w-[190px] h-[250px] relative flex flex-col items-center  cursor-pointer"
+      to={`/${title}`}
+      className="relative flex h-[250px] w-[190px] cursor-pointer flex-col items-center"
     >
       <button onClick={onClickFavorite} className="absolute right-5">
         <FavoriteBorderIcon />
       </button>
       <img
-        className="w-[160px] h-[140px] min-h-[140px] p-4"
+        className="h-[140px] min-h-[140px] w-[160px] p-4"
         src={image}
         alt=""
       />
       <div className="px-4 py-0">
         <h4 className="mb-1">{title}</h4>
-        <p className=" text-gray-400 text-sm">Lowest Ask</p>
-        <b className=" font-mono text-[23px] f">€{base_price}</b>
-        <p className="text-xs bg-gray-100 p-1 w-4/5">min price: €{min_price}</p>
+        <p className="text-sm text-gray-400">Lowest Ask</p>
+        <b className="f font-mono text-[23px]">€{base_price}</b>
+        <p className="w-4/5 bg-gray-100 p-1 text-xs">min price: €{min_price}</p>
       </div>
     </Link>
   );
---
+++ frontend/src/pages/FullItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BreadCrumbs from "../components/BreadCrumbs/BreadCrumbs";
+import { useParams } from "react-router-dom";
+
+const FullItem = () => {
+  const { title } = useParams();
+  return (
+    <div>
+      <BreadCrumbs />
+      {title}
+    </div>
+  );
+};
+
+export default FullItem;
