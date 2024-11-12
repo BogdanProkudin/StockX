2bf4b83a5cfaa0950f7bc718624437e1d8b7a659
Богдан Прокудин <prokudinbogdan50@gmail.com>
2024-11-12 02:54:02 +0200

    filter page #1

---
+++ frontend/src/components/FilterPage/FilterContent.tsx
@@ -0,0 +1,17 @@
+import FilterSelect from "./FilterSelect";
+
+const FilterContent = () => {
+  return (
+    <div className="mt-3 flex h-[400px] w-24 min-w-[1240px] justify-between bg-slate-950">
+      <div className="h-[300px] w-[300px] bg-red-500"></div>
+      <div className="h-[300px] w-[927px] bg-amber-500">
+        <div className="flex justify-between p-2">
+          <span>Home / Search</span>
+          <FilterSelect />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterContent;
---
+++ frontend/src/pages/FilterPage.tsx
@@ -0,0 +1,12 @@
+import FilterContent from "../components/FilterPage/FilterContent";
+import HeaderLayout from "../layout/HeaderLayout";
+
+const FilterPage = () => {
+  return (
+    <div className="min-w-[1240px]">
+      <FilterContent />
+    </div>
+  );
+};
+
+export default FilterPage;
