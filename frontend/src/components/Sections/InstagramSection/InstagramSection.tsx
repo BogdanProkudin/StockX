72d0a2c1cc9af96f0751f1401226d7adb944e36e
HrafovMaksym <mgrafov06@gmail.com>
2024-11-12 23:46:37 +0100

    instagram section

--- frontend/src/components/Sections/InstagramSection/InstagramSection.tsx
+++ frontend/src/components/Sections/InstagramSection/InstagramSection.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAppSelector } from "../../../redux/hook";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
 
 import "swiper/css";
 import TitleSkeleton from "../TitleSkeleton";
@@ -30,13 +32,19 @@ const InstagramSection = () => {
       <div className="mt-5 flex justify-between">
         {instagramSectionItems.length > 0 ? (
           <>
-            <Swiper spaceBetween={10} slidesPerView={4.1}>
+            <Swiper
+              spaceBetween={10}
+              slidesPerView={4.1}
+              slidesPerGroup={4}
+              modules={[Navigation]}
+              navigation
+            >
               {instagramSectionItems.map((obj, id) => (
                 <SwiperSlide key={id}>
-                  <div className="group relative h-[292px] w-[292px] cursor-pointer overflow-hidden rounded-2xl">
+                  <div className="group relative h-[292px] w-[292px] cursor-pointer overflow-hidden rounded-2xl transition-all hover:bg-black">
                     <img
                       src={obj.image}
-                      className="h-full w-full object-cover"
+                      className="h-full w-full object-cover group-hover:opacity-55"
                       alt={obj.image}
                     />
                     <button className="absolute left-24 top-[115px] z-10 hidden rounded-lg border border-white px-2 py-1 text-white hover:bg-white hover:text-black group-hover:block">
