e371df31b66f22e1dcb2db6529033df08c9aaebc
HrafovMaksym <mgrafov06@gmail.com>
2024-11-15 19:28:59 +0100

    modal

--- frontend/src/components/Sections/InstagramSection/InstagramSection.tsx
+++ frontend/src/components/Sections/InstagramSection/InstagramSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAppSelector } from "../../../redux/hook";
 import { Swiper as SwiperType } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -15,44 +15,54 @@ const InstagramSection = () => {
   const instagramSectionItems = useAppSelector(
     (state) => state.homeItems.instagramSectionItems,
   );
+
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [modalSlide, setModaliSlide] = useState(0);
+  const [isModal, setIsModal] = useState(false);
+
+  const [selectedItem, setSelectedItem] = useState<{
+    image: string;
+    data: userCardProps[];
+  } | null>(null);
+
   const totalSlides = instagramSectionItems.length - 1;
 
   const handleSlideChange = (swiper: SwiperType) => {
     setCurrentSlide(swiper.realIndex);
   };
-  const [isModal, setIsModal] = useState(false);
-  const [selectedItem, setSelectedItem] = useState<{
-    image: string;
-    data: userCardProps[];
-  } | null>(null);
-  const openModal = (item: { image: string; data: userCardProps[] }) => {
+  const openModal = (
+    item: { image: string; data: userCardProps[] },
+    id: number,
+  ) => {
     setSelectedItem(item);
     setIsModal(true);
+    setModaliSlide(id);
   };
 
   const closeModal = () => {
     setIsModal(false);
     setSelectedItem(null);
+    setModaliSlide(0);
   };
+
   return (
     <div className="mb-20">
       <h1 className="text-xl font-bold">
-        {instagramSectionItems.length < 1 ? (
+        {instagramSectionItems.length < 2 ? (
           <TitleSkeleton />
         ) : (
           "As Seen On Instagram"
         )}
       </h1>
       <p className="text-sm">
-        {instagramSectionItems.length < 1 ? (
+        {instagramSectionItems.length < 2 ? (
           <TitleSkeleton />
         ) : (
           "Use #GotItOnStockX and you could be featured."
         )}
       </p>
       <div className="relative mt-5 flex justify-between">
-        {instagramSectionItems.length < 1 ? (
+        {instagramSectionItems.length < 2 ? (
           <Skeleton />
         ) : (
           <>
@@ -85,7 +95,7 @@ const InstagramSection = () => {
               {instagramSectionItems.map((obj, id) => (
                 <SwiperSlide key={id}>
                   <div
-                    onClick={() => openModal(obj)}
+                    onClick={() => openModal(obj, id)}
                     className="group relative h-[292px] w-[292px] cursor-pointer overflow-hidden rounded-2xl transition-all duration-100 hover:bg-black"
                   >
                     <img
@@ -101,7 +111,7 @@ const InstagramSection = () => {
               ))}
             </Swiper>
             {isModal && selectedItem && (
-              <Modal closeModal={closeModal} item={selectedItem} />
+              <Modal closeModal={closeModal} modalSlide={modalSlide} />
             )}
           </>
         )}
--- frontend/src/components/Sections/TitleSkeleton.tsx
+++ frontend/src/components/Sections/TitleSkeleton.tsx
@@ -4,13 +4,13 @@ import ContentLoader from "react-content-loader";
 const TitleSkeleton = () => (
   <ContentLoader
     speed={1}
-    width={200}
+    width={250}
     height={20}
-    viewBox="0 0 200 20"
+    viewBox="0 0 250 20"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
   >
-    <rect x="0" y="0" rx="0" ry="0" width="200" height="20" />
+    <rect x="0" y="0" rx="0" ry="0" width="250" height="20" />
   </ContentLoader>
 );
 
