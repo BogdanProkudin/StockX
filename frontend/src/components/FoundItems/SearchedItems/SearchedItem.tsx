dbe12b1e9f69d7fd2106831a979cc9748c45183f
Богдан Прокудин <prokudinbogdan50@gmail.com>
2024-12-28 15:13:50 +0200

    found page search

--- frontend/src/components/FoundItems/SearchedItems/SearchedItem.tsx
+++ frontend/src/components/FoundItems/SearchedItems/SearchedItem.tsx
@@ -27,14 +27,17 @@ const SearchedItem: React.FC<SearchedItemProps> = ({
   };
   return (
     <Link onClick={handleClick} to={`/${name}`}>
-      <div className="flex w-full cursor-pointer items-center border-b-2 border-b-lightGray transition-all duration-100 hover:border-r-8 hover:border-r-green-900">
-        <img className="m-5 h-28 w-32" src={image} />
+      <div className="smallScreen:hover:border-r-4 flex w-full cursor-pointer items-center border-b-2 border-b-lightGray pl-2 pr-2 transition-all duration-100 hover:border-r-8 hover:border-r-green-900">
+        <img
+          className="smallScreen:h-16 smallScreen:w-20 m-5 h-28 w-32"
+          src={image}
+        />
         <div className="flex flex-col">
           <div className="text-[12px] uppercase leading-3">{brand}</div>
-          <div className="text-lg font-bold uppercase leading-5 text-black">
+          <div className="smallScreen:font-semibold smallScreen:text-sm text-lg font-bold uppercase leading-5 text-black">
             {name}
           </div>
-          <div className="m-0 text-[13px] font-bold uppercase leading-5">
+          <div className="smallScreen:text-[11px] m-0 text-[13px] font-bold uppercase leading-5">
             {color}
           </div>
         </div>
