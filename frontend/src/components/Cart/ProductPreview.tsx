f4b7f0be0d4af3fb799c24836dcdaca2f391ad84
HrafovMaksym <mgrafov06@gmail.com>
2025-01-02 20:39:03 +0100

    ship form

--- frontend/src/components/Cart/ProductPreview.tsx
+++ frontend/src/components/Cart/ProductPreview.tsx
@@ -7,7 +7,7 @@ interface ProductPreviewProps {
 }
 const ProductPreview: React.FC<ProductPreviewProps> = ({ image, title }) => {
   return (
-    <div className="relative ml-8 mt-8 flex h-[495px] w-full flex-col items-center">
+    <div className="relative ml-8 mt-8 flex w-full flex-col items-center">
       <button className="absolute left-0 rounded-2xl border border-black px-5 py-2 font-bold text-black transition-all duration-300 ease-in-out hover:bg-black hover:text-white">
         <Link className="flex items-center gap-3" to={`/${title}`}>
           <ArrowLeft size={18} />
@@ -15,7 +15,9 @@ const ProductPreview: React.FC<ProductPreviewProps> = ({ image, title }) => {
         </Link>
       </button>
       <h1 className="my-5 text-center text-xl font-bold">{title}</h1>
-      <img className="h-full" src={image} alt="" />
+      <div className="flex h-4/5 items-center justify-center">
+        <img className="h-[400px]" src={image} alt="" />
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/Cart/ShipForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShipForm = () => {
+  return (
+    <div>
+      <h1 className="mb-3 text-2xl font-bold">Shipping</h1>
+      <p className="text-[#777777]">Enter your shipping details below.</p>
+    </div>
+  );
+};
+
+export default ShipForm;
