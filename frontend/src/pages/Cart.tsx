930c3eabdc330bdb56416bc47ca92ef4951923fd
HrafovMaksym <mgrafov06@gmail.com>
2025-01-04 20:41:29 +0100

    cart

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -21,6 +21,7 @@ import MakeOffer from "../components/Cart/MakeOffer";
 import { useAppDispatch } from "../redux/hook";
 import { setCartPrice } from "../redux/slices/cartSlice";
 import ShipForm from "../components/Cart/ShipForm";
+import ApprovePurchase from "../components/Cart/ApprovePurchase";
 
 const Cart: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -64,6 +65,9 @@ const Cart: React.FC = () => {
     }
   }, [error, navigate]);
   const sizeQuery = searchParams.get("size");
+  const isBid = searchParams.get("isBid");
+  const isConfirm = searchParams.get("isConfirm");
+
   const sizeOrder = ["US", "UK", "CM", "KR", "EU"];
 
   const onClickMakeOffer = () => {
@@ -76,90 +80,102 @@ const Cart: React.FC = () => {
         <Loader />
       ) : (
         <div className="flex h-full min-h-[87vh] justify-between">
-          <ProductPreview image={product?.image} title={product?.title} />
+          <ProductPreview
+            isConfirm={isConfirm}
+            image={product?.image}
+            title={product?.title}
+          />
 
           <div className="flex h-full min-h-[87vh] min-w-[600px] flex-col bg-[#f4f3f1] pt-8">
-            {shipping ? (
-              <div className="px-7">
-                <ShipForm />
-              </div>
+            {isConfirm ? (
+              <ApprovePurchase title={product?.title} size={sizeQuery} />
             ) : (
               <>
-                <div className="flex-1 px-7">
-                  {sizeQuery ? (
-                    <div className="flex h-[450px] flex-col gap-5">
-                      <h1 className="mb-3 text-center text-lg font-bold">
-                        {isMakeOffer ? "Make An Offer" : "Buy Now"}
-                      </h1>
-                      <EditSize
-                        gender={product?.gender}
-                        sizeOrder={sizeOrder[0]}
-                      />
-                      {!isMakeOffer && (
-                        <PriceBlock variants={product?.variants} />
-                      )}
+                {shipping ? (
+                  <div className="px-7">
+                    <ShipForm setShipping={() => setShipping(false)} />
+                  </div>
+                ) : (
+                  <>
+                    <div className="flex-1 px-7">
+                      {sizeQuery ? (
+                        <div className="flex h-[450px] flex-col gap-5">
+                          <h1 className="mb-3 text-center text-lg font-bold">
+                            {isMakeOffer || isBid ? "Make An Offer" : "Buy Now"}
+                          </h1>
+                          <EditSize
+                            gender={product?.gender}
+                            sizeOrder={sizeOrder[0]}
+                          />
+                          {!isMakeOffer && !isBid && (
+                            <PriceBlock variants={product?.variants} />
+                          )}
 
-                      {!isMakeOffer && (
-                        <button
-                          onClick={onClickMakeOffer}
-                          className="flex items-center justify-between rounded-xl bg-white px-5 py-5"
-                        >
-                          <div>
-                            <h4 className="w-[110px] font-semibold">
-                              Make An Offer
-                            </h4>
-                            <span>
-                              Get it for less if a seller accepts your price
+                          {!isMakeOffer && !isBid && (
+                            <button
+                              onClick={onClickMakeOffer}
+                              className="flex items-center justify-between rounded-xl bg-white px-5 py-5"
+                            >
+                              <div>
+                                <h4 className="w-[110px] font-semibold">
+                                  Make An Offer
+                                </h4>
+                                <span>
+                                  Get it for less if a seller accepts your price
+                                </span>
+                              </div>
+                              <span className="rounded-full border border-black p-1">
+                                <ArrowRight size={20} />
+                              </span>
+                            </button>
+                          )}
+                          {(isMakeOffer || isBid) && (
+                            <MakeOffer variants={product?.variants} />
+                          )}
+
+                          <div className="flex items-center justify-between rounded-xl bg-white px-5 py-3">
+                            <div className="flex items-center gap-5">
+                              <Car />
+                              <p>
+                                Standard Shipping <br />
+                                <span className="text-sm text-[#969696]">
+                                  Typically arrives within 14 days
+                                </span>
+                              </p>
+                            </div>
+                            <span
+                              onClick={() => setShipping(true)}
+                              className="cursor-pointer text-sm font-bold text-[#006340]"
+                            >
+                              Add
                             </span>
                           </div>
-                          <span className="rounded-full border border-black p-1">
-                            <ArrowRight size={20} />
-                          </span>
-                        </button>
-                      )}
-                      {isMakeOffer && (
-                        <MakeOffer variants={product?.variants} />
-                      )}
-
-                      <div className="flex items-center justify-between rounded-xl bg-white px-5 py-3">
-                        <div className="flex items-center gap-5">
-                          <Car />
-                          <p>
-                            Standard Shipping <br />
-                            <span className="text-sm text-[#969696]">
-                              Typically arrives within 14 days
-                            </span>
-                          </p>
                         </div>
-                        <span
-                          onClick={() => setShipping(true)}
-                          className="cursor-pointer text-sm font-bold text-[#006340]"
-                        >
-                          Add
-                        </span>
-                      </div>
+                      ) : (
+                        <>
+                          <h1 className="mb-2 text-2xl font-bold">
+                            Select Size
+                          </h1>
+                          <SizeGrid sizeOrder={sizeOrder} />
+                          <SizeBlock variants={product?.variants} />
+                        </>
+                      )}
                     </div>
-                  ) : (
-                    <>
-                      <h1 className="mb-2 text-2xl font-bold">Select Size</h1>
-                      <SizeGrid sizeOrder={sizeOrder} />
-                      <SizeBlock variants={product?.variants} />
-                    </>
-                  )}
-                </div>
-                {sizeQuery ? (
-                  <TotalPrice />
-                ) : (
-                  <div className="border-l border-t border-l-[#cfcfcf] border-t-[#cfcfcf] bg-white px-7 py-5">
-                    <button className="rounded-2xl border border-black px-5 py-2 font-bold text-black transition-all duration-300 ease-in-out hover:bg-black hover:text-white">
-                      <Link
-                        className="flex items-center gap-3"
-                        to={`/${title}`}
-                      >
-                        Cancel
-                      </Link>
-                    </button>
-                  </div>
+                    {sizeQuery ? (
+                      <TotalPrice setShipping={() => setShipping(true)} />
+                    ) : (
+                      <div className="border-l border-t border-l-[#cfcfcf] border-t-[#cfcfcf] bg-white px-7 py-5">
+                        <button className="rounded-2xl border border-black px-5 py-2 font-bold text-black transition-all duration-300 ease-in-out hover:bg-black hover:text-white">
+                          <Link
+                            className="flex items-center gap-3"
+                            to={`/${title}`}
+                          >
+                            Cancel
+                          </Link>
+                        </button>
+                      </div>
+                    )}
+                  </>
                 )}
               </>
             )}
