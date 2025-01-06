702823ab96c1207cfab1ddefa9199fcb8c852b6a
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-01-06 02:23:33 +0200

    edit profile logic

--- frontend/src/layout/ProfileLayout.tsx
+++ frontend/src/layout/ProfileLayout.tsx
@@ -18,7 +18,6 @@ const ProfileLayout = ({ children }: ProfileLayoutProps) => {
   const dispatch = useAppDispatch();
   const { data, isError, isLoading } = useGetUserDataQuery({});
 
-
   useEffect(() => {
     console.log("изменились данные");
 
@@ -34,9 +33,9 @@ const ProfileLayout = ({ children }: ProfileLayoutProps) => {
     } else if (data) {
       dispatch(setUserData(data));
       return;
+    }
+  }, [data, isError]);
 
-
-  
   return (
     <>
       <div className={styles.headerBar}>
