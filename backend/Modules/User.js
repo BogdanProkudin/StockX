97ee1756393d55aca4da5c8098dbbd45096fc9d1
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-01-06 20:42:29 +0200

    fix bugs

--- backend/Modules/User.js
+++ backend/Modules/User.js
@@ -1,50 +1,49 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      require: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      require: true,
-    },
-    firstName: {
-      type: String,
-    },
-    secondName: {
-      type: String,
-    },
-    resetPasswordToken: {
-      type: String,
-    },
-    resetPasswordExpires: {
-      type: String,
-    },
-    passwordResetAttempts: {
-      type: Number,
-    },
-    newPasswordExpires: {
-      type: String,
-    },
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  firstName: {
+    type: String,
+  },
+  secondName: {
+    type: String,
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: String,
+  },
+  passwordResetAttempts: {
+    type: Number,
+  },
+  newPasswordExpires: {
+    type: String,
+  },
 
-    isPurchased: {
-      type: Array,
-      default: [],
+  isPurchased: {
+    type: Array,
+    default: [],
 
     shoeSize: {
       type: String,
     },
     userName: {
       type: String,
-
     },
   },
-  {
-    timestamps: true,
-  }
-);
+
+  timestamps: {
+    type: Boolean,
+  },
+});
 
 export default mongoose.model("User", UserSchema, "StockX");
